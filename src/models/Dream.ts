b2bf9993196c8bb4dd13d548bd81174c98f0a3a0
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-05-30 20:37:06 -0400

    adding interfaces mapper to news relationalships

--- src/models/Dream.ts
+++ src/models/Dream.ts
@@ -5,9 +5,43 @@ export interface Investment {
 }
 
 export type Progress = {
-  progress: number;
+  progress: string;
   state: 'REACHED' | 'WAITING';
 };
+
+export interface QuestionOptions {
+  id: string;
+  answer: string;
+  correct: boolean;
+}
+
+export interface Questions {
+  id: string;
+  question: string;
+  options: QuestionOptions[];
+}
+
+export interface Content {
+  title: string;
+  content: string;
+}
+
+export interface Subtopics {
+  id: string;
+  title: string;
+  progress: Progress[];
+  content: Content[];
+  questions: Questions[];
+}
+
+export interface Topics {
+  id: string;
+  title: string;
+  content: {
+    message: string;
+    subtopics: Subtopics[];
+  };
+}
 
 export interface Dream {
   id: string;
@@ -15,4 +49,5 @@ export interface Dream {
   challenge: string;
   investment: Investment;
   progress: Progress[];
+  topics: Topics[];
 }
